b6f01043be84b6bd879b07ae018eec305f09caf7
pantomash <pantomash@protonmail.com>
2019-06-22 21:42:37 +0200

    init project with spring initializr files

---
+++ src/main/java/pl/pantomash/seatreservation/domain/Table.java
@@ -0,0 +1,4 @@
+package pl.pantomash.seatreservation.domain;
+
+public class Table {
+}
---
+++ src/test/java/pl/pantomash/seatreservation/SeatReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pantomash.seatreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
