92980ebafca4a22a786dc3bd463b5979a055eeee
pantomash <pantomash@protonmail.com>
2019-07-14 17:03:44 +0200

    added profiles - dev and prod. Closes #9.

---
+++ src/main/java/pl/pantomash/seatreservation/configuration/DBConfiguration.java
@@ -0,0 +1,28 @@
+package pl.pantomash.seatreservation.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@SuppressWarnings("unused")
+public class DBConfiguration {
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String devDatabaseConnection() {
+        return "DB CONNCECTION FOR DEV";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+        return "PROD CONNECTION";
+    }
+}
